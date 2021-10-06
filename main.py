9510d3f48309198c0019c5987f6a8f8bf1e8fc68
rileydevine <824055@mystma.org>
2021-10-06 09:00:28 -0500

    submitted

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#define factorial
+def factorial(x):
+
+    #define temp variable
+    total = 1
+    
+    #for loop
+    for i in range(1,x+1):
+        total = total * i
+    return total
+print(factorial(5))
